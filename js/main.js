55846686e96c1e55a139f08f4d4c8c6f3ae6372a
oldmcdonnell <oldmcdonnell@gmail.com>
2024-04-18 15:33:23 -0400

    MVP with a few strtc goal

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
+let nextTurn = document.getElementById("currentTurn")
+let winnerText = document.getElementById("winner")
+let docWinnerX = document.getElementById("winnerX")
+let docWinnerY = document.getElementById("winnerY")
+let winnerX = 0
+let winnerO = 0
 
-//set classname variable
+//setting storage
+
+
+//set boolean for segments of the game
 let newGame = true
+let gameWinner = false
 //set initial turn variable
 let turn = 1;
 //set the array to find the result of the fame
-let resultArray = [null, null, null, null, null, null, null, null, null];
+let resultArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 //switch between x and o everything should be null at the start of the game
 let markXorO = {
     '1': 'X',
-    '-1': '0',
+    '-1': 'O',
     'null': ''
 }
 // find a way to assign an index to the target.id to a variable in the resultArray
@@ -22,73 +32,7 @@ let squareAssign  = {
     cell6:6,
     cell7:7,
     cell8:8,
-    }
-//randomize the first mark played
-function firstMark(){
-    if(Math.random > .5){
-        turn = 1;
-    } else {
-        turn = -1;
-    }
-    return turn
 }
-//get a list of all the squares clicked add event listener
-const squaresClicked = document.getElementsByClassName("square")
-
-for (const square of squaresClicked){
-    square.addEventListener("click", addMarkToBox)
-}
-
-
-
-function addMarkToBox(e){
-    let square = e.target
-    console.log(square.id)
-    let currentMark = document.getElementById(square.id)
-    //square.classList.add("noClick")
-    //remember to remove noclick after reset
-    if (newGame === true){
-        firstMark()
-        handleMove(square)
-        resultArray[squareAssign[square.id]] = markXorO[turn]
-        currentMark.textContent = markXorO[turn]
-        newGame = false
-        turn *= -1;
-        console.log(resultArray)
-    } else {
-        currentMark.textContent = markXorO[turn]
-        console.log(resultArray)
-        turn *= 1;
-    }
-}
-
-
-// Update all impacted state, then call render()
-function handleMove(square) {
-    // obtain index of square
-    const idx = parseInt(square.id.replace('cell', ''));
-    if (
-      // Square already taken
-      resultArray[idx] ||
-      // Game over
-      winner
-    ) return;
-    // Update state (board, turn, winner)
-    resultArray[idx] = turn;
-    turn *= -1;
-    winner = getWinner();
-    // Render updated state
-    //render();
-  }
-
-
-  function getWinner() {
-    for (let i = 0; i < winningCombos.length; i++) {
-      if (Math.abs(resultArray[winningCombos[i][0]] + resultArray[winningCombos[i][1]] + resultArray[winningCombos[i][2]]) === 3) return board[winningCombos[i][0]];
-    }
-    if (resultArray.includes(null)) return null;
-    return 'cat';
-  }
 
 const winningCombos = [
     [0, 1, 2],
@@ -99,4 +43,136 @@ const winningCombos = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-  ];
+];
+    
+//randomize the first mark played
+function firstMark(){
+    if(Math.random() > .5){
+        turn = 1;
+    } else {
+        turn = -1;
+    }
+    console.log("FirstMark turn: "+ turn)
+    return turn
+}
+//get a list of all the squares clicked add event listener
+const squaresClicked = document.getElementsByClassName("square")
+
+for (const square of squaresClicked){
+    square.addEventListener("click", addMarkToBox)
+}
+
+
+function addMarkToBox(e){
+    let square = e.target
+    console.log(square.id)
+    square.classList.add("noClick")
+    //remember to remove noclick after reset
+    if (newGame === true){
+        firstMark()
+        handleMove(square)
+        //resultArray[squareAssign[square.id]] = markXorO[turn]
+        square.textContent = markXorO[turn]
+        newGame = false
+        console.log(resultArray)
+        //turn *= -1
+    } else if(gameWinner === false) {
+        handleMove(square)
+        //resultArray[squareAssign[square.id]] = markXorO[turn]
+        square.textContent = markXorO[turn]
+        console.log(resultArray)
+        console.log('newgame: ', newGame)
+        console.log('turn: ', turn)
+        //turn *= -1;
+    }
+}
+
+
+// Update all impacted state, then call render()
+function handleMove(square) {
+    // obtain index of square
+    const idx = parseInt(square.id.replace('cell', ''));
+    //const idx = parseInt(squareAssign.input)
+    console.log('idx: ', idx)
+    if (resultArray[idx] === 0 && gameWinner === false){
+        resultArray[idx] = turn;
+        nextTurn.textContent = `Next Turn, ${markXorO[turn]}`
+        console.log('Current Turn ', markXorO[turn])
+        turn *= -1;
+        getWinner()
+        console.log('AGW ', markXorO[turn])
+        localStorage.setItem('result array', JSON.stringify({resultArray}))
+        //JSON.parse(localStorage.getItem('data'))
+    }
+  }
+
+function scoreBoard(){
+
+console.log('scoreboard ',markXorO[turn])
+if(markXorO[turn] === 'X'){
+    winnerX++
+    }else{
+    winnerO++
+    }
+
+if(winnerX>=1){
+    docWinnerX.textContent = winnerX
+} 
+if(winnerO>=1){
+    docWinnerY.textContent = winnerO
+}
+console.log("scoreboard triggered")
+}
+
+function catGame(a, obj) {
+    var i = resultArray.length-1;
+    while (i--) {
+       if (a[i] === 0) {
+            true;
+       }
+    }
+    return false;
+}
+
+function getWinner() {
+for (let i = 0; i < winningCombos.length; i++) {
+        //console.log("testing if 3", (Math.abs(resultArray[winningCombos[i][0]] + resultArray[winningCombos[i][1]] + resultArray[winningCombos[i][2]])))
+    if (Math.abs(resultArray[winningCombos[i][0]] + resultArray[winningCombos[i][1]] + resultArray[winningCombos[i][2]]) === 3)
+        {
+        console.log('getWinner: ', resultArray[winningCombos[i][0]])
+        console.log('Winner is: ', markXorO[turn])
+        scoreBoard()
+        winnerText.textContent = `The winner is ${markXorO[turn]}`
+        gameWinner = true
+        console.log(gameWinner)
+        //return resultArray[winningCombos[i][0]];
+        }
+    }
+    if(resultArray.includes(0)){
+        console.log("good to go")
+    } else {
+        console.log("cat")
+        winnerText.textContent = "No winners, Cat game"
+    }
+}
+
+const resetButton = document.getElementById("resetGame")
+resetButton.addEventListener("click", resetFunction)
+
+function resetFunction(){
+    let removeClass = document.getElementsByClassName("square")
+    console.log(removeClass)
+    for (const square of removeClass){
+        square.classList.remove("noClick")
+        square.textContent = ""
+        console.log('square ', square)
+    }
+    
+    console.log(removeClass.classList)
+    resultArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    newGame = true
+    gameWinner = false
+    console.log(resultArray)
+    turn = 1
+    nextTurn.textContent = ''
+}

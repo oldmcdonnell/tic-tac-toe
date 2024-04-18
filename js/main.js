deff10c38ff1471d6beb5d51b5184b481eb5a0a7
oldmcdonnell <oldmcdonnell@gmail.com>
2024-04-17 23:24:36 -0400

    added pseudocode from failed first attempt handing with an array seperate to inithal function

--- js/main.js
+++ js/main.js
@@ -1,84 +1,96 @@
-// // Select all elements with the same first three letters
-// const elements = document.querySelectorAll('[data-name^="cell"]');
 
-// // Loop through the selected elements and add an event listener to each one
-// elements.forEach(element => {
-//   element.addEventListener('click', () => {
-//     // Do something when the element is clicked
-//   });
-// });
-/*-
-
-// Select all elements with the same first three letters
-const cellClicked = document.querySelectorAll('[data-name^="cell"]');
-
-// Loop through the selected elements and add an event listener to each one
-cellClicked.forEach(element => {
-  element.addEventListener('click', () => {
-    console.log("test tclick")
-    // Do something when the element is clicked
-  });
-});
-console.log(cellClicked)
-
--*/
-
-//get by classnane
-let newGame = true;
-let turn = 0;
-let markXorO = ['X', 'O', 'X', 'O', 'X', 'O' , 'X', 'O', 'X']
-
+//set classname variable
+let newGame = true
+//set initial turn variable
+let turn = 1;
+//set the array to find the result of the fame
+let resultArray = [null, null, null, null, null, null, null, null, null];
+//switch between x and o everything should be null at the start of the game
+let markXorO = {
+    '1': 'X',
+    '-1': '0',
+    'null': ''
+}
+// find a way to assign an index to the target.id to a variable in the resultArray
+let squareAssign  = {
+    cell0:0, 
+    cell1:1, 
+    cell2:2,
+    cell3:3,
+    cell4:4,
+    cell5:5,
+    cell6:6,
+    cell7:7,
+    cell8:8,
+    }
+//randomize the first mark played
+function firstMark(){
+    if(Math.random > .5){
+        turn = 1;
+    } else {
+        turn = -1;
+    }
+    return turn
+}
+//get a list of all the squares clicked add event listener
 const squaresClicked = document.getElementsByClassName("square")
 
 for (const square of squaresClicked){
     square.addEventListener("click", addMarkToBox)
 }
 
+
+
 function addMarkToBox(e){
     let square = e.target
     console.log(square.id)
     let currentMark = document.getElementById(square.id)
-    square.classList.add("noClick")
+    //square.classList.add("noClick")
     //remember to remove noclick after reset
     if (newGame === true){
-        turn = Math.round(Math.random())
+        firstMark()
+        handleMove(square)
+        resultArray[squareAssign[square.id]] = markXorO[turn]
         currentMark.textContent = markXorO[turn]
         newGame = false
-        turn++;
-
+        turn *= -1;
+        console.log(resultArray)
     } else {
         currentMark.textContent = markXorO[turn]
-        turn++;
+        console.log(resultArray)
+        turn *= 1;
     }
 }
 
 
-//document.getElementById('board').addEventListener('click', handleMove);
-//playAgainBtn.addEventListener('click', initialize);
+// Update all impacted state, then call render()
+function handleMove(square) {
+    // obtain index of square
+    const idx = parseInt(square.id.replace('cell', ''));
+    if (
+      // Square already taken
+      resultArray[idx] ||
+      // Game over
+      winner
+    ) return;
+    // Update state (board, turn, winner)
+    resultArray[idx] = turn;
+    turn *= -1;
+    winner = getWinner();
+    // Render updated state
+    //render();
+  }
 
-function userIsX(){}
 
-function checkWinningCombos(){}
+  function getWinner() {
+    for (let i = 0; i < winningCombos.length; i++) {
+      if (Math.abs(resultArray[winningCombos[i][0]] + resultArray[winningCombos[i][1]] + resultArray[winningCombos[i][2]]) === 3) return board[winningCombos[i][0]];
+    }
+    if (resultArray.includes(null)) return null;
+    return 'cat';
+  }
 
-let squareAssign  = {
-cell0:0, 
-cell1:1, 
-cell2:2,
-cell3:3,
-cell4:4,
-cell5:5,
-cell6:6,
-cell7:7,
-cell8:8,
-}
-
-
-// let currentTable = [
-//     [null],[],[],
-
-// ]
-
-const winningCombo = [
+const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
---
+++ pseudocode.md
@@ -0,0 +1,25 @@
+##draw the board
+## state for each of the board
+## what state is each of the array is in 
+three sets empty X and O
+
+
+need to reset tthe board, need a reset button
+
+game history?
+store game result in local storage
+how do i place an x on a board
+
+how are we going to manage clicks
+
+event listener for all of the tiles
+
+check for game state after click
+if after click there are winner array
+check if draw happenes
+  
+
+turn management
+
+
+x and y to 1 and negative 1 then use math. asbolute function to find the absolute of the addition of winning sets
